7023d09e8ba0e4424b2cfcd320afc0336c632bd0
Mayday2020 <60267058+Mayday2020@users.noreply.github.com>
2021-05-25 14:50:10 +0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import React from 'react';
+import './App.css';
+import {TodoList} from "./TodoList";
+
+function App() {
+    const tasks1 = [
+        {id: 1, title: "HTML&CSS", isDone: true},
+        {id: 2, title: "JS", isDone: true},
+        {id: 3, title: "ReactJS", isDone: false}
+    ];
+    const tasks2 = [
+        {id: 1, title: "Hello world", isDone: true},
+        {id: 2, title: "I am Happy", isDone: false},
+        {id: 3, title: "Yo", isDone: false}
+    ]
+    return (
+        <div className="App">
+            <TodoList title="What to learn?" tasks={tasks1}/>
+            <TodoList title="Songs" tasks={tasks2}/>
+        </div>
+    );
+}
+export default App;
